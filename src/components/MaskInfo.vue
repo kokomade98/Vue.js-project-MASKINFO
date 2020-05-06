e222cadb70e559634aa4e4aa21b86fd3fd66002b
kokomade98 <c0305sh@naver.com>
2020-05-07 01:13:41 +0900

    MASK INFO

---
+++ src/components/MaskInfo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul class="maskInfo">
+      <li>
+        <span
+          ><img
+            src="https://find-mask.s3.ap-northeast-2.amazonaws.com/img/green.png"
+            alt=""/></span
+        >100개 이상
+      </li>
+      <li>
+        <span
+          ><img
+            src="https://find-mask.s3.ap-northeast-2.amazonaws.com/img/yellow.png"
+            alt=""
+          /> </span
+        >30개 ~ 99개
+      </li>
+      <li>
+        <span
+          ><img
+            src="https://find-mask.s3.ap-northeast-2.amazonaws.com/img/red.png"
+            alt=""
+          /> </span
+        >2개 ~ 29개
+      </li>
+      <li>
+        <span
+          ><img
+            src="https://find-mask.s3.ap-northeast-2.amazonaws.com/img/gray.png"
+            alt=""
+          /> </span
+        >0개 ~ 1개
+      </li>
+    </ul>
+    <p class="info">YEUNGJIN UNIVERSITY WDJ 조승현</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/SearchLocation.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="searchLocation">
+    <div>
+      <input
+        class="searchBox"
+        type="text"
+        placeholder="목적지를 입력해주세요."
+        v-model="searchValue"
+        @keydown="checkEnter"
+      /><img src="../imgs/search.png" alt="" @click="locationSend" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      searchValue: '',
+    };
+  },
+  methods: {
+    locationSend() {
+      if (this.searchValue != '') this.$emit('send_location', this.searchValue);
+    },
+    checkEnter(keyValue) {
+      if (keyValue.key === 'Enter') this.locationSend();
+    },
+  },
+};
+</script>
+
+<style></style>
